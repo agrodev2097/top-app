3d70b373e413975ae6b1f0abd2d73654101a67f4
Artem <a.shevchuk@helsi.me>
2021-11-19 14:41:12 +0200

    Added review component

--- components/TextArea/TextArea.tsx
+++ components/TextArea/TextArea.tsx
@@ -1,9 +1,16 @@
 import { TextAreaProps } from './TextArea.props';
 import styles from './TextArea.module.css';
 import cn from 'classnames';
+import { ForwardedRef, forwardRef } from 'react';
 
-export const TextArea = ({ className, ...props }: TextAreaProps): JSX.Element => {
+export const TextArea = forwardRef(({ error, className, ...props }: TextAreaProps, ref: ForwardedRef<HTMLTextAreaElement>): JSX.Element => {
 	return (
-		<textarea className={cn(className, styles.textarea)}{...props} />
+		<div className={cn(styles.textareaWrapper, className)}>
+			<textarea className={cn(styles.textarea, {
+				[styles.error]: error
+			})} ref={ref} {...props} />
+			{error && <span className={styles.errorMessage}>{error.message}</span>}
+		</div>
+
 	);
-};
+});
